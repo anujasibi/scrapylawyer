625e295ae37b071c8005d417ab4fa075351655c3
anujasibi <45849232+anujasibi@users.noreply.github.com>
2018-12-19 17:58:20 +0530

    Update lawyers.py

--- scrapylaw/scrapylawyer/spiders/lawyers.py
+++ scrapylaw/scrapylawyer/spiders/lawyers.py
@@ -8,4 +8,17 @@ class LawyersSpider(scrapy.Spider):
     start_urls = ['http://http://www.avvo.com/all-lawyers/ny/new_york.html/']
 
     def parse(self, response):
-        pass
+        name= response.css('.name::text').extract()
+        license= response.css('.time::attr(datetime)').extract()
+        avvo_rating= response.css('.smallclass::text').extract()
+        user_rating= response.css('.small::text').extract()
+        images= response.css('image.attr(image)').extract()
+        about=response.css('card::text').extract()
+        
+        
+    for item in zip(name,license,avvo_rating,user_rating,images,about)
+         scrapped_info={
+             'name':item[0],
+             'license':item[1]
+         }
+        yield scrapped info
